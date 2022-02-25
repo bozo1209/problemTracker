6e8ca378f12af41455ac308186e42769b64cae72
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2022-02-25 16:55:37 +0100

    add user roles

---
+++ src/main/java/com/bozo/problemtracker/enums/UserRoles.java
@@ -0,0 +1,5 @@
+package com.bozo.problemtracker.enums;
+
+public enum UserRoles {
+    LEADER_POSTER, POSTER, LEADER_RECEIVER, RECEIVER
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,9 +8,10 @@ insert into market values(3, 'fr', 1)
 insert into market values(4, 'ro', 2)
 insert into market values(5, 'br', 3)
 
-insert into users values(1, 'one', 'pass', 'a')
-insert into users values(2, 'two', 'pass', 'a')
-insert into users values(3, 'three', 'pass', 'a')
+insert into users values(1, 'one', 'pass', 'LEADER_POSTER')
+insert into users values(2, 'two', 'pass', 'POSTER')
+insert into users values(3, 'three', 'pass', 'LEADER_RECEIVER')
+insert into users values(4, 'four', 'pass', 'RECEIVER')
 
 --insert into note values(1, '2021-12-21', 'issue', 'aa11', 'open', 1, 1)
 --insert into note values(2, '2021-12-21', 'issue', 'aa11', 'open', 2, 2)
